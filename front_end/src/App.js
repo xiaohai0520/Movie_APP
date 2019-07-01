99537ff331a670e38be79cb290626d45f32e84cf
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-07-01 13:10:27 -0400

    Add files via upload

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,16 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Info from './components/Info/Info';
+import Logo from './components/Logo/Logo';
+import SearchForm from './components/SearchForm/SearchForm';
 import './App.css';
-import SearchForm from './components/searchForm/searchForm';
 
 
+const serverurl = 'http://localhost:8080/';
 
-class App extends React.Component {
-
-    render() {
-        return (
-          <SearchForm />
-        )
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
     }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        info: {
+            name: '',
+            year: '',
+            budget: '',
+            score: '',
+            actor0: '',
+            actor1: '',
+            director: '',
+            genre: '',
+            recommendation: [], 
+
+        }
+   
+    }
+
+
+      this.changeInfo = this.changeInfo.bind(this);
+  }
+
+  changeInfo = (data) => {
+      this.setState({ info: data });
+  }
+
+  onInputChange = (event) => {
+
+      this.setState({ info: { name: event.target.value } });
+
+  }
+
+  onButtonSubmit = () => {
+      fetch(serverurl + '/:name')
+          .then(res => res.json())
+          .then(data => {
+              this.changeInfo(data);
+          })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+         <Particles className='particles'
+          params={particlesOptions}
+        />
+        <div>
+            <Logo />
+            <SearchForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+            />
+            <Info info={this.state.info} />
+
+
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+//app.get('/:name', function (req, res) {
+//    var params = req.params
+//    res.send(params)
+//})
